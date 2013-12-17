557b32a18b51cc08b85ba8cf4068809f346104d3
Ben Morgan <bmorgan.warwick@gmail.com>
2013-12-17 17:50:35 +0000

    Add subproject for Boost.Iterator tests

---
+++ Source/Iterator/basic_iterator.cpp
@@ -0,0 +1,21 @@
+// basic_iterator - trivial test of boost::iterator
+//
+// Copyright (c) 2013 by Ben Morgan <bmorgan.warwick@gmail.com> 
+// Copyright (c) 2013 by The University of Warwick
+//
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+// Standard Library
+
+// Third Party
+// - Boost
+
+// This Project
+
+
+int main(int argc, char *argv[]) {
+  
+  return 0;
+}
