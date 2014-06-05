963f29fc92eadb7a6e118db473bfc97ffedc345b
Ben Morgan <bmorgan.warwick@gmail.com>
2014-06-05 16:23:00 +0100

    Add simple examples of statement > newline parsing
    Correct skipping is needed to correctly parse significant newlines
    in input text.
    
    Provide two examples of parsing statements separated by newlines.
    In sn_parsing, demonstrate use of qi's blank_type to only skip proper
    spaces and tabs. In sn_list_parsing, demonstrate use of skip
    directive to allow continuation across newlines, in this case parsing
    a list with each list element on a separate line.

---
+++ Source/Spirit/sn_parsing.cpp
@@ -0,0 +1,73 @@
+// sn_parsing - basic parsing of "statement > newline" structure
+
+#define BOOST_SPIRIT_DEBUG
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <boost/foreach.hpp>
+#include <boost/spirit/include/qi.hpp>
+#include <boost/spirit/include/support_istream_iterator.hpp>
+#include <boost/fusion/include/std_pair.hpp>
+namespace qi = boost::spirit::qi;
+
+typedef std::pair<std::string, int> Statement;
+typedef std::vector<Statement> Document;
+
+// Statement grammar
+// Insist on blank_type because statements should only be agnostic to
+// spaces and tabs
+template <typename Iterator>
+struct StatementGrammar : qi::grammar<Iterator,Statement(),qi::blank_type> {
+  StatementGrammar() : StatementGrammar::base_type(statement) {
+    identifier %= qi::alpha >> *(qi::alnum | qi::char_('_'));
+    statement = identifier > '=' > qi::int_;
+    BOOST_SPIRIT_DEBUG_NODE(identifier);
+    BOOST_SPIRIT_DEBUG_NODE(statement);
+  }
+
+  qi::rule<Iterator,std::string()> identifier;
+  qi::rule<Iterator,Statement(),qi::blank_type> statement;
+};
+
+// Document grammar
+// Bracket statements so that preceeding/trailing whitespace are chomped
+// Finish with eoi to give complete grammar
+template <typename Iterator>
+struct DocumentGrammar : qi::grammar<Iterator,Document(), qi::blank_type> {
+  DocumentGrammar() : DocumentGrammar::base_type(document) {
+    document %= qi::omit[*qi::space]
+                >> statement % +qi::char_("\n")
+                >> qi::omit[*qi::space]
+                > qi::eoi;
+    BOOST_SPIRIT_DEBUG_NODE(document);
+  }
+
+  StatementGrammar<Iterator> statement;
+  qi::rule<Iterator,Document(), qi::blank_type> document;
+};
+
+int main(int argc, const char *argv[])
+{
+  std::ifstream input(argv[1]);
+  input.unsetf(std::ios::skipws);
+
+
+  boost::spirit::istream_iterator first(input);
+  boost::spirit::istream_iterator last;
+
+  Document doc;
+  DocumentGrammar<boost::spirit::istream_iterator> g;
+
+  bool result = qi::phrase_parse(first, last, g, qi::blank, doc);
+
+  if (result) {
+    std::cout << "good parse" << std::endl;
+    BOOST_FOREACH(const Statement& s, doc) {
+      std::cout << "id: " << s.first << ", value: " << s.second << std::endl;
+    }
+  } else {
+    std::cerr << "bad parse" << std::endl;
+  }
+
+  return 0;
+}
